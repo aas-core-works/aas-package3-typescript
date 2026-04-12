e7a86f686f96f26737216cbf95058c18907f53e0
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2026-04-12 14:44:11 +0200

    Improves test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    include: ['test/**/*.test.ts']
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary", "html", "lcov"],
+      thresholds: {
+        lines: 85,
+        functions: 85,
+        branches: 75,
+        statements: 85
+      }
+    }
   }
 });
